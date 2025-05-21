a15bff5c891aed41b7f7d56bee18b5e0e6dd61e9
sm praneeth kumar <smparneethkumr431gmail.com>
2025-05-21 15:19:43 +0530

    practicing react

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -6,19 +6,55 @@ export class ContactUs extends Component {
         firstName :"",
         lastName :"",
         email :"",
-        phone :""
+        phone :"",
+        firstNameErr:"",
+        lastNameErr :"",
+        emailErr:"",
+        phoneErr:""
+    }
+
+    handleValidation = ()=>{
+    let firstNameErr = ""
+    let lastNameErr = ""
+    let emailErr = ""
+    let phoneErr = ""
+
+     if(this.state.firstName.length < 3){
+        firstNameErr= "first should be atleast 3 chars"; 
+        console.log(this.state.firstNameErr);
+          
+    }
+     if(this.state.lastName.length <1){
+        lastNameErr= "first should be atleast 3 chars";
+    }
+     if(!this.state.email.includes("@") ){
+        emailErr= "first should be atleast 3 chars";
+    }
+     if(this.state.phone.length !=10){
+        phoneErr= "first should be atleast 3 chars";
+    }
+
+    if (firstNameErr || lastNameErr || emailErr || phoneErr){
+        this.setState({firstNameErr,lastNameErr,emailErr,phoneErr})
+    }else{
+         this.setState({firstNameErr,lastNameErr,emailErr,phoneErr})
+    } 
     }
 
     handleChange=(event)=> {
         const {name,value} = event.target
         this.setState({[name]: value })
-        console.log(this.state);
+      
         
     }
     handleSubmit = (event)=>{
         event.preventDefault()
-    }
+        this.handleValidation()
+    };
   render() {
+
+    
+      const {firstName,lastName,email,phone,firstNameErr,lastNameErr,emailErr,phoneErr} = this.state
     return (
       <>
     <h2>ContactUS</h2>
@@ -28,41 +64,46 @@ export class ContactUs extends Component {
         <form action="">
             <div className="col-md-4">
                <input type="text" 
-               class="form-control" 
+               className="form-control" 
                name='firstName' 
                onChange={this.handleChange} 
-               value={this.state.firstName}
+               value={firstName}
                 id="exampleFormControlInput1" 
-                placeholder="Enter your first name"/>  
+                placeholder="Enter your first name"/> 
+                <span>{firstNameErr}</span> <br />
             </div>   
             <div className="col-md-4">
                  <input type="text" 
-                 class="form-control" 
+                 className="form-control" 
                  name='lastName' 
                  onChange={this.handleChange} 
-                 value={this.state.lastName}
+                 value={lastName}
                   id="exampleFormControlInput2" 
                   placeholder="Enter your last name"/>
+                  <span>{lastNameErr}</span>
             </div>
             <div className="col-md-4">
                 <input type="email" 
-                class="form-control"
+                className="form-control"
                  name='email'
                   onChange={this.handleChange} 
-                  value={this.state.email} 
+                  value={email} 
                   id="exampleFormControlInput3"
                    placeholder="Enter your email"/>
+                   <span>{emailErr}</span>
             </div>
             <div className="col-md-4">
                 <input type="number"
-                 class="form-control"
+                 className="form-control"
                   name='phone'
                    onChange={this.handleChange} 
-                   value={this.state.phone} 
+                   value={phone} 
                    id="exampleFormControlInput4" 
-                   placeholder="Enter your number "/><br/>
+                   placeholder="Enter your number "/>
+                   <span>{phoneErr}</span>
+                   <br/>
 
-                   <button type="button" class="btn btn-primary">submit</button>
+                   <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>submit</button>
             </div>
         </form>
         </div>
