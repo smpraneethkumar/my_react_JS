b16f9f4e0347006406f12f8672a0fc204ab248c9
sm praneeth kumar <smparneethkumr431gmail.com>
2025-05-23 21:02:49 +0530

    useeffect

--- src/Components/Hooks/UseStateHooks.jsx
+++ src/Components/Hooks/UseStateHooks.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function UseStateHooks() {
+const [count,setCount]= useState(0)
+
+const incrementHandler =() => {
+  setCount(preCount => preCount +1 )
+  setCount(preCount => preCount +1 )
+}
+const decrementHandler =() => {
+  setCount(preCount => preCount  -1 )
+}
+
+
   return (
-    <div>ff</div>
+    <div>
+      <h2>count :{count} </h2>
+      <button type="button" className="btn btn-primary me-5" onClick={incrementHandler}>increment</button>
+      <button type="button" className="btn btn-danger" onClick={decrementHandler}>decrement</button>
+    </div>
   )
 }
 
